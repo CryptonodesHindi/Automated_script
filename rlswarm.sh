f38d86df8cc890d998454abf9129e04c4b8ee2c0
kumar <r.palei33@gmail.com>
2025-04-20 11:38:49 +0530

    updating the command

--- rlswarm.sh
+++ rlswarm.sh
@@ -6,6 +6,7 @@ GREEN="\e[32m"
 YELLOW="\e[33m"
 NC="\e[0m"
 
+
 cat << "EOF"
  ██████╗██████╗ ██╗   ██╗██████╗ ████████╗ ██████╗     ███╗   ██╗ ██████╗ ██████╗ ███████╗    ██╗  ██╗██╗███╗   ██╗██████╗ ██╗
 ██╔════╝██╔══██╗╚██╗ ██╔╝██╔══██╗╚══██╔══╝██╔═══██╗    ████╗  ██║██╔═══██╗██╔══██╗██╔════╝    ██║  ██║██║████╗  ██║██╔══██╗██║
@@ -25,11 +26,12 @@ echo -e "${YELLOW}YouTube: ${GREEN}https://www.youtube.com/@CryptonodesHindi${NC
 echo -e "${YELLOW}Medium: ${GREEN}https://medium.com/@cryptonodehindi${NC}"
 echo "======================================================="
 
+
 SWARM_DIR="$HOME/rl-swarm"
 TEMP_DATA_PATH="$SWARM_DIR/modal-login/temp-data"
 HOME_DIR="$HOME"
 
-cd "$HOME"
+cd $HOME
 
 if [ -f "$SWARM_DIR/swarm.pem" ]; then
     echo -e "${BOLD}${YELLOW}You already have an existing ${GREEN}swarm.pem${YELLOW} file.${NC}\n"
@@ -48,7 +50,7 @@ if [ -f "$SWARM_DIR/swarm.pem" ]; then
             rm -rf "$SWARM_DIR"
 
             echo -e "${BOLD}${YELLOW}[✓] Cloning fresh repository...${NC}"
-            cd "$HOME" && git clone https://github.com/gensyn-ai/rl-swarm/ > /dev/null 2>&1
+            cd $HOME && git clone https://github.com/gensyn-ai/rl-swarm> /dev/null 2>&1
 
             mv "$HOME_DIR/swarm.pem" rl-swarm/
             mv "$HOME_DIR/userData.json" rl-swarm/modal-login/temp-data/ 2>/dev/null
@@ -57,7 +59,7 @@ if [ -f "$SWARM_DIR/swarm.pem" ]; then
         elif [ "$choice" == "2" ]; then
             echo -e "${BOLD}${YELLOW}[✓] Removing existing folder and starting fresh...${NC}"
             rm -rf "$SWARM_DIR"
-            cd "$HOME" && git clone https://github.com/gensyn-ai/rl-swarm/ > /dev/null 2>&1
+            cd $HOME && git clone https://github.com/gensyn-ai/rl-swarm > /dev/null 2>&1
             break
         else
             echo -e "\n${BOLD}${RED}[✗] Invalid choice. Please enter 1 or 2.${NC}"
@@ -65,8 +67,7 @@ if [ -f "$SWARM_DIR/swarm.pem" ]; then
     done
 else
     echo -e "${BOLD}${YELLOW}[✓] No existing swarm.pem found. Cloning repository...${NC}"
-    cd "$HOME" && [ -d rl-swarm ] && rm -rf rl-swarm
-    git clone https://github.com/gensyn-ai/rl-swarm/ > /dev/null 2>&1
+    cd $HOME && [ -d rl-swarm ] && rm -rf rl-swarm; git clone https://github.com/gensyn-ai/rl-swarm> /dev/null 2>&1
 fi
 
 cd rl-swarm || { echo -e "${BOLD}${RED}[✗] Failed to enter rl-swarm directory. Exiting.${NC}"; exit 1; }
